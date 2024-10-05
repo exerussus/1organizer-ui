4ea26aa1e4b88bc9936798ec85d9b29535df5cb4
exerussus <solovyov.production@gmail.com>
2024-10-05 04:53:52 +0300

    Auto updated to version 1.0.16

--- Scripts/Pooling/AssetPool.cs
+++ Scripts/Pooling/AssetPool.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Exerussus._1OrganizerUI.Scripts.Pooling
@@ -8,6 +7,7 @@ namespace Exerussus._1OrganizerUI.Scripts.Pooling
     public class AssetPool<T> where T : MonoBehaviour, ILoadAsset
     {
         private readonly Dictionary<string, AddressablePoolObject<T>> _assetPool = new();
+        private Dictionary<string, Queue<Job>> _jobs = new();
         
         public async void Initialize(string name, string path)
         {
@@ -16,45 +16,72 @@ namespace Exerussus._1OrganizerUI.Scripts.Pooling
             _assetPool[name] = newPool;
         }
 
-        public void GetAndExecute(string name, string path, Action<T> action) => GetAndExecuteAsync(name, path, action);
+        public void GetAndExecute(string name, string path, Action<T> action)
+        {
+            if (_jobs.ContainsKey(name))
+            {
+                _jobs[name].Enqueue(new Job(name, path, Vector3.zero, Quaternion.identity, action));
+                return;
+            }
+            
+            if (HasPool(name)) action(_assetPool[name].GetObject(Vector3.zero, Quaternion.identity));
+            else
+            {
+                var job = new Queue<Job>();
+                _jobs[name] = job;
+                job.Enqueue(new Job(name, path, Vector3.zero, Quaternion.identity, action));
+                InitializePool(name, path);
+            }
+        }
+        
         public void GetAndExecute(string name, string path, Vector3 position, Action<T> action)
         {
-            if (name == null) Debug.LogError("Asset name is null 2");
-            GetAndExecuteAsync(name, path, position, action);
-        }
-        public void GetAndExecute(string name, string path, Vector3 position, Quaternion quaternion, Action<T> action) => GetAndExecuteAsync(name, path, position, quaternion, action);
-        
-        private async void GetAndExecuteAsync(string name, string path, Action<T> action)
-        {
-            var pool = await TryInitializePool(name, path);
-            action(pool.GetObject(Vector3.zero, Quaternion.identity));
-        }
-        
-        private async void GetAndExecuteAsync(string name, string path, Vector3 position, Action<T> action)
-        {
-            if (name == null) Debug.LogError("Asset name is null 3");
-            var pool = await TryInitializePool(name, path);
-            action(pool.GetObject(position, Quaternion.identity));
-        }
-        
-        private async void GetAndExecuteAsync(string name, string path, Vector3 position, Quaternion quaternion, Action<T> action)
-        {
-            var pool = await TryInitializePool(name, path);
-            action(pool.GetObject(position, quaternion));
-        }
-
-        private async Task<AddressablePoolObject<T>> TryInitializePool(string name, string path)
-        {
-            if (!_assetPool.TryGetValue(name, out var pool))
+            if (_jobs.ContainsKey(name))
             {
-                pool = new AddressablePoolObject<T>();
-                
-                await pool.InitPrefab(name, path, false, 5);
-                _assetPool[name] = pool;
+                _jobs[name].Enqueue(new Job(name, path, position, Quaternion.identity, action));
+                return;
             }
-
-            return pool;
+            
+            if (HasPool(name)) action(_assetPool[name].GetObject(position, Quaternion.identity));
+            else
+            {
+                var job = new Queue<Job>();
+                _jobs[name] = job;
+                job.Enqueue(new Job(name, path, position, Quaternion.identity, action));
+                InitializePool(name, path);
+            }
         }
+
+        public void GetAndExecute(string name, string path, Vector3 position, Quaternion quaternion, Action<T> action)
+        {
+            if (_jobs.ContainsKey(name))
+            {
+                _jobs[name].Enqueue(new Job(name, path, position, quaternion, action));
+                return;
+            }
+            
+            if (HasPool(name)) action(_assetPool[name].GetObject(position, quaternion));
+            else
+            {
+                var job = new Queue<Job>();
+                _jobs[name] = job;
+                job.Enqueue(new Job(name, path, position, quaternion, action));
+                InitializePool(name, path);
+            }
+        }
+
+        private async void InitializePool(string name, string path)
+        {
+            var pool = new AddressablePoolObject<T>();
+                
+            await pool.InitPrefab(name, path, false, 5);
+            _assetPool[name] = pool;
+
+            foreach (var job in _jobs[name]) job.Action(_assetPool[name].GetObject(job.Position, job.Quaternion));
+            _jobs.Remove(name);
+        }
+        
+        private bool HasPool(string name) => _assetPool.ContainsKey(name);
 
         public T Get(string name)
         {
@@ -67,8 +94,22 @@ namespace Exerussus._1OrganizerUI.Scripts.Pooling
             if (!_assetPool.TryGetValue(asset.AssetName, out var pool)) Debug.LogError("Ассет не проинициализирован.");
             pool.ReleaseObject(asset);
         }
-    }
 
+        private struct Job
+        {
+            public Job(string name, string path, Vector3 position, Quaternion quaternion, Action<T> action)
+            {
+                Position = position;
+                Quaternion = quaternion;
+                Action = action;
+            }
+
+            public Vector3 Position;
+            public Quaternion Quaternion;
+            public Action<T> Action;
+        }
+    }
+    
     public interface ILoadAsset
     {
         public string AssetName { get; }
