cbe8019c968c88f9d921abcd2ce7a3c87067c587
exerussus <solovyov.production@gmail.com>
2025-05-23 14:29:11 +0300

    Auto updated to version 1.0.91

---
+++ Example.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Games.CardEngine.Source.Features.BoardFeature.Scripts.MonoBehaviours
+{
+    public class AssetProvider : ScriptableObject
+    {
+        public AssetReferenceT<GroupPack>[] groups;
+        public Dictionary<string, AssetReference> references;
+    }
+    
+    public class GroupPack : ScriptableObject
+    {
+        public ReferencePack[] packs;
+    }
+
+    public class ReferencePack
+    {
+        public string id;
+        public AssetReference reference;
+    }
+}
